05907c30f2ff2e90e9e4fe72b29d78155ab64791
Nick Fox-Gieg <nick@fox-gieg.com>
2017-04-06 19:23:00 -0400

    first

---
+++ Source/latkUnreal/latkUnreal.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "latkUnreal.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, latkUnreal, "latkUnreal" );
